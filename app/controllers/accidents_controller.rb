f61b1b7e56ad88fb18f467505b028656ef5db9ad
Henry Miguel Guzmán Escorcia <hguzman10@gmail.com>
2014-08-26 15:02:19 -0500

    Prueba Mapas

--- app/controllers/accidents_controller.rb
+++ app/controllers/accidents_controller.rb
@@ -76,6 +76,6 @@ class AccidentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def accident_params
-      params.require(:accident).permit(:informe, :transito, :transit_id, :gravedad, :latitud, :longitud, :direccion, :localidad, :fecha_ocurrencia_at, :fecha_levantamiento_at, :clase_accidente, :choque_con, :objeto_fijo, :area, :sector, :zona, :diseno, :clima, :calle, :numero, :ciudad, :departamento)
+      params.require(:accident).permit(:informe, :transito, :transit_id, :gravedad, :latitud, :longitud, :direccion, :localidad, :fecha_ocurrencia_at, :fecha_levantamiento_at, :clase_accidente, :choque_con, :objeto_fijo, :area, :sector, :zona, :diseno, :clima)
     end
 end
